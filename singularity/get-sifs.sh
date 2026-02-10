697b28c1e4a0da741ed24e2b5f3538775530ae9a
rolandocoto-cr <rolandocoto@gmail.com>
2026-02-10 11:47:41 +0000

    Create get-sifs.sh

---
+++ singularity/get-sifs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://rcweb.dartmouth.edu/RCoto/installers-dali-2025/asr-w2v2-lm-python310.sif
+wget https://rcweb.dartmouth.edu/RCoto/installers-dali-2025/asr-silero-python309.sif
